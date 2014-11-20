6cf19a7fdf7843eb8a1f8071eefe9391d9959e24
matthieudelaro <matthieudelaro@gmail.com>
2014-11-20 19:31:03 +0100

    Implemented as much as I understood what is the goal of ex 4. Some more work is to be done.

--- tp1ex4/tp1ex4/main.c
+++ tp1ex4/tp1ex4/main.c
@@ -38,36 +38,38 @@ int main(int argc, char *argv[], char *envp[])
         fprintf(stderr, "Pas d'utilisateur loge ?\n");
         exit(1);
     } else {
-        printf("found user : %s", utilisateur);
+        printf("found user : %s\n", utilisateur);
     }
     
     /*
      * Creation du tube et appel de fork().
      */
     pipe(desctube);
+    idp = fork();
     
-    //    ...
-    
+    if (idp == -1) {
+        fprintf(stderr, "Erreur avec fork()\n");
+        exit(1);
+    }
     /* Ici le fils !
      * Envoi par le fils ‡ l'utilisateur du message contenu dans le tube
      */
-    if (idp == 0) {
+    else if (idp == 0) {
         /*
          * Redirection de l'entree standard vers le tube
          */
-//        ...
-//        ...
-//        ...
+        dup2(desctube[1], 1); // redirect stdout
         
         /*
          * Pour voir la sortie sur l'ecran, on ferme le tube en ecriture.
          */
-//        ...
+        close(desctube[1]);
+        
         /*
          * Envoi du mail
          */
         execl("/usr/bin/mail", "mail", utilisateur, 0);//working on mac os
-        perror("Exec a echoue !");
+        perror("Exec a echoue !\n");
         exit(1);
     } else {
         
@@ -76,6 +78,7 @@ int main(int argc, char *argv[], char *envp[])
          */
         printf("Surveillez votre BAL, si tout se passe bien, un message vous sera envoye \n");
         close(desctube[0]);
+        fp = fdopen(desctube[1], "w");
         //    fp = fdopen(...,... );
         fprintf(fp, "C'est le message que vous attendez, cher utilisateur.\n");
         fclose(fp);
@@ -83,9 +86,66 @@ int main(int argc, char *argv[], char *envp[])
         /*
          * Le pere attend la terminaison du fils
          */
-        //...
+        int status;
+        wait(&status);
         exit(0);
     }
 
 }
+
+//Using pipes as file streams, fdopen()
+//
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <sys/types.h>
+//
+//#define MAXLINE         512
+//
+//int main(void) {
+//    char line[MAXLINE];
+//    FILE *fpin  = {0};
+//    FILE *fpout = {0};
+//    pid_t child = -1;
+//    int fd[2] = {0};
+//    int i = 0;
+//    
+//    pipe(fd);
+//    
+//    if((child = fork()) == -1) {
+//        perror("fork");
+//        return 1;
+//    }
+//    
+//    if(child == 0) {
+//        close(fd[0]);
+//        
+//        fpout = fdopen(fd[1], "w");
+//        if(fpout == NULL) {
+//            fprintf(stderr, "Error - fdopen(child)\n");
+//            return 1;
+//        }
+//        
+//        for(i = 0; i < 10; i++)
+//            fprintf(fpout, "%s\n", "jeronimooo...");
+//        
+//        fclose(fpout);
+//        return 0;
+//    } else {
+//        close(fd[1]);
+//        
+//        fpin = fdopen(fd[0], "r");
+//        if(fpin == NULL) {
+//            fprintf(stderr, "Error - fdopen(parent)\n");
+//            return 1;
+//        }
+//        
+//        while(fgets(line, MAXLINE, fpin) != NULL)
+//            printf("%d: %s", i++, line);
+//        
+//        fclose(fpin);
+//    }
+//    
+//    return 0;
+//}
+
 
