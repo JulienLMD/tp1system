b7c1e968cd1a190cbabc1e8d0a0532006a0c7410
Julien Lamandé <Green@MacBook-Pro-de-Julien.local>
2014-11-21 11:09:12 +0100

    ex1_added

--- tp1ex1/main.c
+++ tp1ex1/main.c
@@ -10,22 +10,15 @@ void sig_fpe(int sig)
     printf("\n");
 }
 
-//
+
 int main(int argc, char *argv[], char *envp[])
 {
     signal(SIGPIPE, sig_fpe);
     int tube[2];
-    if (-1) {
-        printf("bonjour\n");
-    }
-    if (0) {
-        printf("non bonjour\n");
-    }
     printf("bonjour\n");
     pipe(tube);
     close(tube[0]);
-    write(tube[1],"AB",2);
-    if (-1) {
+    if (write(tube[1],"AB",2) == -1) {
         perror("erreurdsrdd");
     }
     exit(0);
