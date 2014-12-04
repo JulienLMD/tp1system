76990729e44266af74f43683a8bcfec8a01defc0
matthieudelaro <matthieudelaro@gmail.com>
2014-12-04 11:31:33 +0100

    securing system calls in tp1ex4

--- tp1ex4/tp1ex4/main.c
+++ tp1ex4/tp1ex4/main.c
@@ -6,19 +6,6 @@
 //  Copyright (c) 2014 Matthieu. All rights reserved.
 //
 
-//#include <stdio.h>
-//
-//int main(int argc, const char * argv[]) {
-//    // insert code here...
-//    printf("Hello, World!\n");
-//    system("echo Hello");
-////    echo "Message Body" | /usr/bin/mail -s "Message Title" matthieudelaro@gmail.com
-//    return 0;
-//}
-
-///
-/* Envoi d'un mail ‡ un utilisateur via un tube */
-/*** Remplacer les points par les instructions ou les parametres manquants ***/
 
 #include <errno.h>
 #include <stdio.h>
@@ -44,9 +31,13 @@ int main(int argc, char *argv[], char *envp[])
     /*
      * Creation du tube et appel de fork().
      */
-    pipe(desctube);
-    idp = fork();
+    if (pipe(desctube))
+    {
+        fprintf (stderr, "Pipe failed.\n");
+        return EXIT_FAILURE;
+    }
     
+    idp = fork();
     if (idp == -1) {
         fprintf(stderr, "Erreur avec fork()\n");
         exit(1);
@@ -59,25 +50,35 @@ int main(int argc, char *argv[], char *envp[])
         /*
          * Redirection de l'entree standard vers le tube
          */
-//        dup2(desctube[1], 1); // redirect stdout
-        close(0);
-        dup(desctube[0]); // redirect stdout
-        close(desctube[0]);
-//        close(desctube[1]);
-        
-//        read()
+        if(close(0) == -1)
+        {
+            fprintf (stderr, "close(0) failed.\n");
+            return EXIT_FAILURE;
+        }
+        if (dup(desctube[0]) < 0) // redirect stdout
+        {
+            fprintf (stderr, "Dup failed.\n");
+            return EXIT_FAILURE;
+        }
+        if(close(desctube[0]) == -1)
+        {
+            fprintf (stderr, "close(desctube[0]) failed.\n");
+            return EXIT_FAILURE;
+        }
         
         /*
          * Pour voir la sortie sur l'ecran, on ferme le tube en ecriture.
          */
-        close(desctube[1]);
+        if(close(desctube[1]) == -1)
+        {
+            fprintf (stderr, "close(desctube[1]) failed.\n");
+            return EXIT_FAILURE;
+        }
         
         /*
          * Envoi du mail
          */
-                execl("/usr/bin/mail", "mail", utilisateur, 0);//working on mac os
-//        execl("/usr/bin/mail", "mail", utilisateur, utilisateur);//working on mac os
-//        execl("/usr/bin/mail", "mail", "matthieudelaro@bruins-macbook-air.local", 0, "-s tp1ex2");//working on mac os
+        execl("/usr/bin/mail", "mail", utilisateur, 0);//working on mac os
         perror("Exec a echoue !\n");
         exit(1);
     } else {
@@ -86,10 +87,18 @@ int main(int argc, char *argv[], char *envp[])
          * Le pere ecrit le message dans le tube
          */
         printf("Surveillez votre BAL, si tout se passe bien, un message vous sera envoye \n");
-        close(desctube[0]);
+        if(close(desctube[0]) == -1)
+        {
+            fprintf (stderr, "close(desctube[0]) failed.\n");
+            return EXIT_FAILURE;
+        }
         fp = fdopen(desctube[1], "w");
-        //    fp = fdopen(...,... );
-        fprintf(fp, "C'est le message que vous attendez, cher utilisateur.\n");
+        if (fp == NULL) {
+            fprintf (stderr, "fdopen(desctube[1], \"w\") failed.\n");
+            return EXIT_FAILURE;
+        } else {
+            fprintf(fp, "C'est le message que vous attendez, cher utilisateur.\n");
+        }
         fclose(fp);
         
         /*
@@ -101,60 +110,5 @@ int main(int argc, char *argv[], char *envp[])
     }
 
 }
-
-//Using pipes as file streams, fdopen()
-//
-//#include <stdio.h>
-//#include <unistd.h>
-//#include <sys/types.h>
-//
-//#define MAXLINE         512
-//
-//int main(void) {
-//    char line[MAXLINE];
-//    FILE *fpin  = {0};
-//    FILE *fpout = {0};
-//    pid_t child = -1;
-//    int fd[2] = {0};
-//    int i = 0;
-//    
-//    pipe(fd);
-//    
-//    if((child = fork()) == -1) {
-//        perror("fork");
-//        return 1;
-//    }
-//    
-//    if(child == 0) {
-//        close(fd[0]);
-//        
-//        fpout = fdopen(fd[1], "w");
-//        if(fpout == NULL) {
-//            fprintf(stderr, "Error - fdopen(child)\n");
-//            return 1;
-//        }
-//        
-//        for(i = 0; i < 10; i++)
-//            fprintf(fpout, "%s\n", "jeronimooo...");
-//        
-//        fclose(fpout);
-//        return 0;
-//    } else {
-//        close(fd[1]);
-//        
-//        fpin = fdopen(fd[0], "r");
-//        if(fpin == NULL) {
-//            fprintf(stderr, "Error - fdopen(parent)\n");
-//            return 1;
-//        }
-//        
-//        while(fgets(line, MAXLINE, fpin) != NULL)
-//            printf("%d: %s", i++, line);
-//        
-//        fclose(fpin);
-//    }
-//    
-//    return 0;
-//}
 
 
